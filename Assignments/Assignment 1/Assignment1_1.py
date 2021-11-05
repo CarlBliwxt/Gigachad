fd2e63286868425e6bff1e669035f0c067007000
CarlBliwxt <carl.pingis@gmail.com>
2021-11-05 10:56:37 +0100

    -update

--- Assignments/Assignment 1/Assignment1_1.py
+++ Assignments/Assignment 1/Assignment1_1.py
@@ -4,33 +4,32 @@
 import turtle
 import random
 
-#skärm för outputen
-
-def jump(t, x, y):
+def jump(t, x, y): # Sets the direcition of the turtle
     t.penup()
     t.goto(x,y)
+    t.speed(0)
     t.pendown()
 
-def make_turtle(x, y, visible =True):
+def make_turtle(x, y, visible =True): # Creates a turtle and sets speed 
     t = turtle.Turtle()
     if visible==False:
         t.hideturtle()
         t.speed(0)
     jump(t, x, y)
     return t
-def random_turtle(side=500):
+
+def random_turtle(side=500): # Creates a random turtle inside a square
     t = make_turtle(random.randint(-side/2, side/2),
                     random.randint(-side/2,side/2))
     t.setheading(random.randint(0, 359)) 
      
 
-def rectangle(x, y, width, height, color=""):
+def rectangle(x, y, width, height, color=""): # Draws a recttangle 
     t = make_turtle(x,y) 
     t.fillcolor(color)
-    t.speed(0)
     t.hideturtle()
     t.begin_fill()
-    t.forward(width)
+    t.forward(width) # Can be done with for loop, like assigment 3. 
     t.left(90)
     t.forward(height)
     t.left(90)
@@ -40,13 +39,13 @@ def rectangle(x, y, width, height, color=""):
     t.end_fill()
 
 def tricolore(x, y, h):
-    colors = ["blue", "white", "red"]
+    colors = ["blue", "white", "red"] 
     w = h/2
     for i in range(3):
         rectangle(x + w*i, y, w, h, colors[i]) 
 
 
-def pentagram(x, y, side, color):
+def pentagram(x, y, side, color): # Creates a pentagram 
     t = make_turtle(x,y) # Creates it at (x,y)
     t.color(color)
     t.speed(0) 
@@ -58,7 +57,7 @@ def pentagram(x, y, side, color):
         t.left(180 - 36)
     t.end_fill()
 
-def generator(x, y, amount, color):
+def generator(x, y, amount, color): # Uses the pentagram to create amount, and what color
     distans=120
     for i in range(amount):
         pentagram(x, y, distans, color)
--- Assignments/Assignment 1/Assignment1_2.py
+++ Assignments/Assignment 1/Assignment1_2.py
@@ -3,28 +3,25 @@
 # A program that draws the vietnamese_flag
 import turtle
 
-#skärm för outputen
-
-def jump(t, x, y):
+def jump(t, x, y): # Sets directions to the tutrtle 
     t.penup()
     t.goto(x,y)
+    t.speed(0)
     t.pendown()
 
-def make_turtle(x, y, visible =True):
+def make_turtle(x, y, visible =True): # Sets, position and speed to the turtle. 
     t = turtle.Turtle()
     if visible==False:
         t.hideturtle()
-        t.speed(0)
+        t.speed(3)
     jump(t, x, y)
     return t
 
-def rectangle(x, y, width, height, color=""):
+def rectangle(x, y, width, height, color=""): # Draws a rectangle 
     t = make_turtle(x,y) 
     t.fillcolor(color)
-    t.speed(0)
-    t.hideturtle()
     t.begin_fill()
-    t.forward(height)
+    t.forward(height) # This can be done with a for loop, like in assigment 3, but left the code like the orignal 
     t.left(90)
     t.forward(width)
     t.left(90)
@@ -33,34 +30,24 @@ def rectangle(x, y, width, height, color=""):
     t.forward(width)
     t.end_fill()
 
-def vietnamese_flag(x, y, height):
+def vietnamese_flag(x, y, height):# Function that creates the vietnamese flag, uses rectangle and pentagram
     colors = ["red","red","red"]
     width = height/2
     rectangle(x, y, width, height, "red")
     pentagram(x+300, y+230, height*0.3,"yellow") 
     
-
-
-
-def pentagram(x, y, side, color):
+def pentagram(x, y, side, color): # Creates a pentagram
     t = make_turtle(x,y) # Creates it at (x,y)
-    t.speed(0) 
     t.hideturtle() 
     t.color(color)
     t.begin_fill()
-    
     t.setheading(252)
-    for i in range(5):
+    for i in range(5): # For loop to draw a penta gram 
         t.forward(side)
-        t.left(180 - 36)
+        t.left(180 - 36) # 36 degreees every turn 
     t.end_fill()
 
-def star(x, y, height, color):
-    distans=120
-    for i in range(amount):
-        pentagram(x, y, distans, color)
-        x=x+distans
-
+# Script, calls the functions to create the flag, 
 vietnamese_flag(-300,-150, 600)
 turtle.done()
 
